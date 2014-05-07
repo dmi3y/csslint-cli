4827fd277a8cb942eb1d14f92d006b81553eb940
dmi3y <lapshukov@gmail.com>
2014-05-07 00:30:14 -0700

    further work to simplify rc
    fixing nodeunits

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
     nodeunit: {
-      files: ['test/helper_test.js']
+      files: ['test/**/*_test.js']
     },
     jshint: {
       options: {
