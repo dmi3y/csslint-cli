920bdb223dcdee731911e6006da6d7196bf2a5fe
dmi3y <lapshukov@gmail.com>
2014-05-10 00:44:26 -0700

    breaking and fixing

--- test/rc_test.js
+++ test/rc_test.js
@@ -8,38 +8,34 @@ exports.rc = {
     'shuffleToRulesets': function(test) {
         var
             exp = c.footprint({
-               "a/b/c/":{
-                  "rules":{},
-                  "files":[
-                     "a/b/c/1.css",
-                     "a/b/c/d/1.css",
-                     "a/b/c/d/2.css"
-                  ]
-               },
-               "a/":{
-                  "rules":{},
-                  "files":[
-                     "a/1.css",
-                     "a/2.css",
-                     "a/b/1.css"
-                  ]
-               }
+                "a/b/c": {
+                    "files": ["a/b/c/1.css", "a/b/c/d/1.css", "a/b/c/d/2.css"],
+                    "rules": {}
+                },
+                "a": {
+                    "files": ["a/1.css", "a/2.css", "a/b/1.css"],
+                    "rules": {}
+                }
             }),
-            datain = {
-                '.rc':[
-                    'a/.rc',
-                    'a/b/c/.rc'
-                ],
-                '.css':[
-                    'a/1.css',
-                    'a/2.css',
-                    'a/b/1.css',
-                    'a/b/c/1.css',
-                    'a/b/c/d/1.css',
-                    'a/b/c/d/2.css'
-                ]
-            },
-            res = c.footprint('shuffleToRulesets', datain);
+            rcs = [
+                {
+                    base: 'a/b/c',
+                    rules: {}
+                },
+                {
+                    base: 'a',
+                    rules: {}
+                }
+            ],
+            files = [
+                'a/1.css',
+                'a/2.css',
+                'a/b/1.css',
+                'a/b/c/1.css',
+                'a/b/c/d/1.css',
+                'a/b/c/d/2.css'
+            ],
+            res = c.footprint('shuffleToRulesets', rcs, files);
 
         test.expect(1);
         test.equal(exp, res);
