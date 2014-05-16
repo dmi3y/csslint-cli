5836c9bfd4f6f46009a211c69127fbcfd2dc61c6
dmi3y <lapshukov@gmail.com>
2014-05-16 01:56:30 -0700

    adding istanbul

--- script/coverage.sh
+++ script/coverage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-istanbul cover ./node_modules/grunt-contrib-nodeunit/node_modules/nodeunit/bin/nodeunit -- ./test/*_test.js
+node ./node_modules/istanbul/lib/cli.js cover ./node_modules/grunt-contrib-nodeunit/node_modules/nodeunit/bin/nodeunit -- ./test/*_test.js
 
 exit 0
