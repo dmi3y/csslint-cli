ec616dbed0b97b7323d926956bdbfb525ce0cd83
dmi3y <lapshukov@gmail.com>
2014-05-21 12:09:20 -0700

    reporter wrapup

--- reporter/reporter-console-default.js
+++ reporter/reporter-console-default.js
@@ -12,7 +12,7 @@ var
     pr = require('../lib/printer'),
     ck = require('chalk');
 
-module.exports = function(result, file/*, options*/) {
+module.exports = function(result, file, options) {
     var
         msgLen = result.messages.length,
         msg,
@@ -27,12 +27,17 @@ module.exports = function(result, file/*, options*/) {
     });
 
 
+    // out.push('');
+
     for(i = 0; i < msgLen; i += 1) {
 
         msg = result.messages[i];
-        coord = ck.gray('L') + ck.bold(msg.line) + ck.gray(':C') + ck.bold(msg.col);
-        out.push('|   ' + pr[msg.type](msg.message));
-        out.push('|   ' + coord + ck.gray(' . . . ') + ck.bold(msg.evidence.trim()));
+        if ( !msg.rollup ) {
+
+            coord = ck.gray('L') + ck.bold.yellow(msg.line) + ck.gray(':C') + ck.bold.yellow(msg.col);
+            out.push('|   ' + coord + ck.gray(' . . . ') + ck.bold.magenta(msg.evidence.trim()));
+        }
+        out.push('|   ' + pr[msg.type](msg.message) + ck.gray('(' + msg.rule.id + ')'));
         // out.push('|    |');
         out.push('|');
         type[msg.type] = '';
@@ -44,16 +49,17 @@ module.exports = function(result, file/*, options*/) {
         color = type.hasOwnProperty('error')? 'red': 'yellow';
 
         out.unshift('|');
-        out.unshift('+-' + ck[color].bold(file.path + ': ') +  msgLen + ' issues:');
-        out.unshift('');
+        out.unshift('+-' + ck[color].bold(file.path + ': ') +  msgLen + ' issue(s):');
         out.push('+-' + ck[color].bold(file.path));
+        out.push('');
 
     } else if ( file.isEmpty ) {
-        out.push(pr.warning(file.path + ' - is empty.'));
-    } else {
-        out.push(pr.ok(file.path));
+        out.push(pr.warning(file.path + ' - is empty.\n'));
+    } else if ( !options.quiet ) {
+        out.push(pr.ok(file.path) + '\n');
     }
-    out.push('');
 
-    pr.log(out.join('\n'));
+    if ( out.length ) {
+        pr.log(out.join('\n'));
+    }
 };
